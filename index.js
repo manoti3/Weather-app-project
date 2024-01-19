6c8e28ffa92b945b41098bd520c2e3a7830d2d86
manoti3 <hillarymanoti0@gmail.com>
2024-01-19 13:24:44 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+*{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+body{
+    background:url('images/946901-the-11-best-free-ocean-wallpaper.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    overflow: hidden;
+    height: 100vh;
+    font-family: 'poppins', sans-serif;
+}
